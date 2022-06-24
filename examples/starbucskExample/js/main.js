905800354550a78a02894c9feffc05fc93bf5f06
jaehkim <rebuilding1303@gmail.com>
2022-06-24 18:05:10 +0900

    [feat] study 6일차 22.06.24 - starbucksExample  notice/promotion section 작업   - 버튼 스타일, swiper 슬라이드 적용, 이미지 순차적 보임 작업, 이미지 슬라이드 작업 진행

--- examples/starbucskExample/js/main.js
+++ examples/starbucskExample/js/main.js
@@ -49,3 +49,47 @@ window.addEventListener('scroll', _.throttle( function() {
             opacity: 1
         })
     });
+
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical',
+    // autoplay: true,
+    loop: true
+});
+
+
+new Swiper('.promotion .swiper-container', {
+    // direction: 'horizontal',
+    slidesPerView: 3, //한 번에 보여줄 슬라이드 개수
+    spaceBetween: 10,  // 슬라이드 사이 여백ㄴ
+    centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+    loop: true,
+    // autoplay: {
+    //     delay: 5000
+    // },
+    pagination: {
+        el: '.promotion .swiper-pagination',
+        clickable: true
+    },
+    navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next'
+    }
+});
+
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener('click', function() {
+    isHidePromotion = !isHidePromotion
+    if (isHidePromotion) {
+        // 숨김 처리
+        promotionEl.classList.add('hide');
+    } else {
+        // 보임 처리
+        promotionEl.classList.remove('hide');
+    }
+})
