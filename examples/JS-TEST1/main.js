93854be8e788b2c6e30641526b378c75bac3a032
jaehkim <rebuilding1303@gmail.com>
2022-07-04 13:44:36 +0900

    study main.js

--- examples/JS-TEST1/main.js
+++ examples/JS-TEST1/main.js
@@ -139,3 +139,131 @@ function timeout(cb) {
 timeout(() => {
     console.log('Done!')
 })
+
+
+//  생성자 함수 (prototype)
+const heropy = {
+    firstName: 'Heropy',
+    lastName: 'Park',
+    getFullName: function () {
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+const neo = {
+    firstName: 'Neo',
+    lastName: 'Smith',
+    getFullName: function () {
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+console.log('heropy1', heropy)
+console.log('heropy2', heropy.getFullName())
+
+console.log('neo1', neo)
+console.log('neo2', neo.getFullName())
+
+// 생성자 함수 (prototype) 예제
+//// 리터럴 방식
+function User(first, last) {
+    this.firstName = first
+    this.lastName = last
+    // this.getFullName = function () {
+    //     return `${this.firstName} ${this.lastName}`
+    // }
+}
+
+User.prototype.getFullName = function () {
+    // 메모리에 딱 1번만 만들어짐
+    // 각 인스턴스는 이것을 참조만 하는 것
+    return `${this.firstName} ${this.lastName}`
+}
+
+const heropy3 = new User('Heropy', 'Park')
+const amy3 = new User('Amy', 'Clarke')
+const neo3 = new User('Neo', 'Smith')
+console.log('heropy3', heropy3.getFullName())
+console.log('amy3', amy3.getFullName())
+console.log('neo3', neo3.getFullName())
+
+
+// this
+// 일반(Normal) 함수는 호출 위치에 따라 this 정의
+// 화살표(Arrow) 함수는 자신이 선언된 함수 범위에서 this 정의
+
+const heropy4 = {
+    name: 'Heropy',
+    normal: function () {
+        console.log(this.name)
+    },
+    arrow : () => {
+        console.log(this.name)
+    }
+}
+
+console.log('heropy4 n', heropy4.normal())
+// console.log('heropy4 a', heropy4.arrow()) // 찾지못함
+
+const amy4 = {
+    name: 'Amy',
+    normal: heropy4.normal,
+    arrow: heropy4.arrow
+}
+
+console.log('amy4 n', amy4.normal())
+//console.log('amy4 a', amy4.arrow()) // 찾지못함
+
+function User1(name) {
+    this.name = name
+}
+
+User1.prototype.normal = function () {
+    console.log(this.name)
+}
+
+User1.prototype.arrow = function () {
+    console.log(this.name)
+}
+
+const temp1 = new User1('Heropy')
+console.log('temp1 n', temp1.normal())
+console.log('temp1 a', temp1.arrow())
+
+
+// const timer1 = { 
+//     name: 'Heropy!',
+//     timeout: function () {
+//         setTimeout(function () {
+//             console.log(this.name)
+//         }, 2000)
+//     }
+// }
+
+const timer1 = { 
+    name: 'Heropy!timer',
+    timeout: function () {
+        setTimeout(() => {
+            console.log(this.name)
+        }, 2000)
+    }
+}
+
+timer1.timeout()
+
+
+
+class User {
+    constructor(first, last) {
+        this.firstName = first
+        this.lastName = last
+    }
+
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`
+    }
+
+}
+const heropy6 = new User('Heropy', 'Park')
+const amy6 = new User('Amy', 'Clarke')
+const neo6 = new User('Neo', 'Smith')
